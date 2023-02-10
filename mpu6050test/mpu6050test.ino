28f47af7235de6b9253927fa5f0ff0f8d9366876
ilem35 <102783767+ilem35@users.noreply.github.com>
2023-02-10 22:00:14 +0800

    Add files via upload

--- mpu6050test/mpu6050test.ino
+++ mpu6050test/mpu6050test.ino
@@ -1,6 +1,8 @@
 #include <Adafruit_MPU6050.h>
 Adafruit_MPU6050 mpu;
 
+#include "getAngle.h"
+
 void setup(void) {
   Serial.begin(115200);
   while (!Serial){
@@ -16,7 +18,7 @@ void setup(void) {
   }
   Serial.println("MPU6050 Found!");
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);//设置加速度
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 }
@@ -33,7 +35,15 @@ void loop() {
   Serial.print(a.acceleration.z);
   Serial.println(" m/s^2");
 
-  Serial.print("各方向角加速度 X: ");
+  float angle_x = getAngle(a.acceleration.x,a.acceleration.z);
+  float angle_y = getAngle(a.acceleration.y,a.acceleration.z);
+  Serial.print("各方向角度 X: ");
+  Serial.print(angle_x);
+  Serial.print(", Y: ");
+  Serial.print(angle_y);
+  Serial.println(" °");
+
+  Serial.print("各方向角速度 X: ");
   Serial.print(g.gyro.x);
   Serial.print(", Y: ");
   Serial.print(g.gyro.y);
