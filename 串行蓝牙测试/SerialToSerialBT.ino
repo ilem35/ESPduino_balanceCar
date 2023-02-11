2b11dae5267511e570d6a87bcefe328614f83973
ilem35 <102783767+ilem35@users.noreply.github.com>
2023-02-11 12:50:57 +0800

    Add files via upload

---
+++ 串行蓝牙测试/SerialToSerialBT.ino
@@ -0,0 +1,18 @@
+#include "BluetoothSerial.h"
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32蓝牙测试");//Bluetooth设备命名
+  Serial.println("设备启动，现在您可以将ESP32与其他设备的蓝牙配对！");
+}
+
+void loop() {
+  if (Serial.available()) {     //若串口可用,蓝牙广播从串口发送的消息（大多数时候其实没有消息，也就无发送）
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {   //若蓝牙可用,串口打印从蓝牙发送的消息
+    Serial.write(SerialBT.read());
+  }
+  delay(20);//方便调试,正式运行时去掉此延时
+}
